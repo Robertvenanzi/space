e6844c2993937489fa2cbc3c20ada6cb5444cb80
robertvenanzi <robertgvenanzi@gmail.com>
2019-01-10 23:55:25 -0800

    imported particles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,6 +7,58 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles
+          params={{
+            "particles": {
+              "number": {
+                "value": 160,
+                "density": {
+                  "enable": false
+                }
+              },
+              "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                  "speed": 4,
+                  "size_min": 0.3
+                }
+              },
+              "line_linked": {
+                "enable": false
+              },
+              "move": {
+                "random": true,
+                "speed": 1,
+                "direction": "top",
+                "out_mode": "out"
+              }
+            },
+            "interactivity": {
+              "events": {
+                "onhover": {
+                  "enable": true,
+                  "mode": "bubble"
+                },
+                "onclick": {
+                  "enable": true,
+                  "mode": "repulse"
+                }
+              },
+              "modes": {
+                "bubble": {
+                  "distance": 250,
+                  "duration": 2,
+                  "size": 0,
+                  "opacity": 0
+                },
+                "repulse": {
+                  "distance": 400,
+                  "duration": 4
+                }
+              }
+            }
+          }} />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
