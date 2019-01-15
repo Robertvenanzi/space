9e11e73da4b0850f28457b1dcf9baa75001e30ec
robertvenanzi <robertgvenanzi@gmail.com>
2019-01-14 23:54:39 -0800

    moved particles to a component

--- src/App.js
+++ src/App.js
@@ -1,78 +1,14 @@
 import React, { Component } from 'react';
-import Particles from 'react-particles-js';
-import logo from './logo.svg';
+import StarParticles from './components/Particles/StarParticles';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Particles
-          params={{
-            "particles": {
-              "number": {
-                "value": 160,
-                "density": {
-                  "enable": false
-                }
-              },
-              "size": {
-                "value": 3,
-                "random": true,
-                "anim": {
-                  "speed": 4,
-                  "size_min": 0.3
-                }
-              },
-              "line_linked": {
-                "enable": false
-              },
-              "move": {
-                "random": true,
-                "speed": 1,
-                "direction": "top",
-                "out_mode": "out"
-              }
-            },
-            "interactivity": {
-              "events": {
-                "onhover": {
-                  "enable": true,
-                  "mode": "bubble"
-                },
-                "onclick": {
-                  "enable": true,
-                  "mode": "repulse"
-                }
-              },
-              "modes": {
-                "bubble": {
-                  "distance": 250,
-                  "duration": 2,
-                  "size": 0,
-                  "opacity": 0
-                },
-                "repulse": {
-                  "distance": 400,
-                  "duration": 4
-                }
-              }
-            }
-          }} />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+        <StarParticles />
+
       </div>
     );
   }
