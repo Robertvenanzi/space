795d99fe7dd032de7ca0cdd588e69f0c9fb92ead
robertvenanzi <robertgvenanzi@gmail.com>
2019-01-15 23:34:03 -0800

    created Title.js with header and logo

---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './title.sass'
+
+const Title = () => {
+    return (
+        <header className="titleContainer">
+            <h1 className="titleLogo"> Spaceman </h1>
+        </header>
+    )
+}
+
+export default Title;
