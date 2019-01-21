07ac45b455829f4c8fc9e75a65f94785a9394997
robertvenanzi <robertgvenanzi@gmail.com>
2019-01-20 18:14:57 -0800

    added main nav, home scss, prop types, mapStateToProps, and connect()

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import { toggleNavMenuOpen, toggleNavMenuClose } from '../../actions/navToggleActions.js';
+import { toggleSearchBarOpen, toggleSearchBarClose } from '../../actions/toggleSearchBarActions.js';
 import StarParticles from '../Particles/StarParticles';
 import Title from '../Title/Title'
-import './home.sass';
+import MainNav from '../MainNav/MainNav'
+import './home.scss';
 
 class Home extends Component {
+
     render() {
+
+        const navContent = `
+        <a href="#home"><li>Home</li></a>
+        `;
         return (
             <div className="homeContainer">
                 <StarParticles />
+                <MainNav content={navContent} handleNavMenuToggle={{ toggleOff: this.props.toggleNavMenuClose, toggleOn: this.props.toggleNavMenuOpen, state: this.props.navToggle }} handleSearchBarToggle={{ toggleOff: this.props.toggleSearchBarClose, toggleOn: this.props.toggleSearchBarOpen, state: this.props.toggleSearchBar }} />
                 <Title />
             </div>
         )
     }
 }
 
-export default Home;
+// Check propTypes for Redux
+toggleNavMenuOpen.propTypes = {
+    toggleNavMenu: PropTypes.func.isRequired,
+    toggle: PropTypes.bool.isRequired
+}
+
+toggleNavMenuClose.propTypes = {
+    toggleNavMenu: PropTypes.func.isRequired,
+    toggle: PropTypes.bool.isRequired
+}
+
+toggleSearchBarOpen.propTypes = {
+    toggleSearchBar: PropTypes.func.isRequired,
+    payload: PropTypes.bool.isRequired
+}
+
+toggleSearchBarClose.propTypes = {
+    toggleSearchBar: PropTypes.func.isRequired,
+    payload: PropTypes.bool.isRequired
+}
+
+// Map state to props
+const mapStateToProps = state => {
+    return { navToggle: state.navToggle.toggleNavMenu, toggleSearchBar: state.searchBarToggle.toggleSearchBar }
+};
+
+export default connect(mapStateToProps, { toggleNavMenuOpen, toggleNavMenuClose, toggleSearchBarOpen, toggleSearchBarClose })(Home);
